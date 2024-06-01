4bcb5610d890c856c2cc4b1364989347d9a5f9ef
Thiago Figueira <tsantosfigueira@outlook.com>
2024-06-01 19:55:34 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember(n){
+    if (n <= 1) {
+        return n;
+    } else {
+        let fib = [0, 1];
+        for (let i = 2; i <= n; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+        return fib[n];
+    }
+}
